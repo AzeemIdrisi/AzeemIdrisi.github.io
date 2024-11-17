2f55079707e57c584dceafe825d14dbb69d8f4e0
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-11-17 21:26:32 +0530

    Open Links In New Tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,31 +19,31 @@ function Navbar() {
         </a>
       </div>
       <div className="m-8 flex  items-center justify-center gap-4 ">
-        <a href="https://www.linkedin.com/in/azeem5202">
+        <a href="https://www.linkedin.com/in/azeem5202" target="_blank">
           <FaLinkedinIn
             size={24}
             className=" hover:text-white transition-all duration-300"
           />
         </a>
-        <a href="https://github.com/AzeemIdrisi">
+        <a href="https://github.com/AzeemIdrisi" target="_blank">
           <FaGithub
             size={24}
             className=" hover:text-white transition-all duration-300"
           />
         </a>
-        <a href="http://www.instagram.com/azeem_5202">
+        <a href="http://www.instagram.com/azeem_5202" target="_blank">
           <FaInstagram
             size={24}
             className=" hover:text-white transition-all duration-300"
           />
         </a>
-        <a href="https://www.twitter.com/Azeem_5202">
+        <a href="https://www.twitter.com/Azeem_5202" target="_blank">
           <FaXTwitter
             size={24}
             className=" hover:text-white transition-all duration-300"
           />
         </a>
-        <a href="https://www.facebook.com/Azeem.5202">
+        <a href="https://www.facebook.com/Azeem.5202" target="_blank">
           <FaFacebook
             size={24}
             className=" hover:text-white transition-all duration-300"

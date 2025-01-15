5fdbf969179d133e8ee4949afa464aa8edd4461c
Azeem Idrisi <azeemidrisi@protonmail.com>
2025-01-16 00:38:47 +0530

    Added socials to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaXTwitter } from "react-icons/fa6";
 import laptopImg from "../assets/Laptop.png";
 import { motion } from "framer-motion";
 function Contact() {
@@ -54,18 +55,47 @@ function Contact() {
               transition={{ duration: 0.5, delay: 0.3 }}
               className="font-semibold text-neutral-400 text-sm"
             >
-              Linktree
+              Socials
             </motion.h6>
-            <motion.p
+            <motion.div
               whileInView={{ opacity: 1, y: -0 }}
               initial={{ opacity: 0, y: -100 }}
               transition={{ duration: 0.5, delay: 0.3 }}
               className="my-1 text-xl"
             >
-              <a href="https://linktr.ee/AzeemIdrisi" target="_blank">
-                linktr.ee/AzeemIdrisi
-              </a>
-            </motion.p>
+              <div className="mt-4 flex  items-center justify-center gap-4 ">
+                <a href="https://www.linkedin.com/in/azeem5202" target="_blank">
+                  <FaLinkedinIn
+                    size={30}
+                    className=" hover:text-white transition-all duration-300"
+                  />
+                </a>
+                <a href="https://github.com/AzeemIdrisi" target="_blank">
+                  <FaGithub
+                    size={30}
+                    className=" hover:text-white transition-all duration-300"
+                  />
+                </a>
+                <a href="http://www.instagram.com/azeem_5202" target="_blank">
+                  <FaInstagram
+                    size={30}
+                    className=" hover:text-white transition-all duration-300"
+                  />
+                </a>
+                <a href="https://www.twitter.com/Azeem_5202" target="_blank">
+                  <FaXTwitter
+                    size={30}
+                    className=" hover:text-white transition-all duration-300"
+                  />
+                </a>
+                <a href="https://www.facebook.com/Azeem.5202" target="_blank">
+                  <FaFacebook
+                    size={30}
+                    className=" hover:text-white transition-all duration-300"
+                  />
+                </a>
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
